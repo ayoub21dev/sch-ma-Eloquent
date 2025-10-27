1ac73e2470bf1bde506f0adac153e4a52e5437b6
ayoub21dev <Ayoubcode941@gmail.com>
2025-10-27 12:29:47 +0000

    start the proejct

---
+++ blog-laravel/database/migrations/2025_10_27_113654_create_artical_tag_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artical_tag', function (Blueprint $table) {
+            $table->foreignId('article_id')->constrained()->cascadeOnDelete();
+             $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+             $table->primary(['article_id', 'tag_id']); // évite les doublons
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artical_tag');
+    }
+};
